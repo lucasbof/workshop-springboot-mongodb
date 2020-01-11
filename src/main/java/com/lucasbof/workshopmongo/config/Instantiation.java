f58b0cb03958816a7fc0506f18cd29b60d5b72e5
Lucas Bof <lucasfbof@gmail.com>
2020-01-11 19:08:54 -0300

    Obtendo um post por id

--- src/main/java/com/lucasbof/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lucasbof/workshopmongo/config/Instantiation.java
@@ -38,7 +38,7 @@ public class Instantiation implements CommandLineRunner{
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 		
---
+++ src/main/java/com/lucasbof/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.lucasbof.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasbof.workshopmongo.domain.Post;
+import com.lucasbof.workshopmongo.repository.PostRepository;
+import com.lucasbof.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
